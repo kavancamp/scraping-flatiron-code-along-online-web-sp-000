14124a1cf06fd19a4399a9bea8a316edd7a8736c
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-09 22:34:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
-
+require 'pry'
 require_relative './course.rb'
 
 class Scraper
-  
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +15,10 @@ class Scraper
       end
     end
   end
-  
+
+  def get_page
+
+      Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
 end
-
-
-
+Scraper.new.get_page
